b17d75524c9212c72b0601b9dd15844917e58682
Michael Pratt <michael@pratt.im>
2015-12-13 14:10:27 -0800

    Basic test of bindings

--- tests/cpp/test.cc
+++ tests/cpp/test.cc
@@ -1,7 +1,38 @@
+#include <stdint.h>
 #include <stdio.h>
+
 #include "tests/cpp/person.pb.hpp"
 
+void person(void) {
+    Person p;
+    p.set_id(20);
+
+    uint8_t buf[256] = { 0 };
+    if (!p.Encode(buf, 256)) {
+        printf("Person encode failed");
+        return;
+    }
+
+    auto p2 = Person::Decode(buf, 256);
+    printf("id = %d\n", p2.get_id());
+}
+
+void phone_number(void) {
+    Person_PhoneNumber p;
+    p.set_type(nanopb::Person_PhoneType_WORK);
+
+    uint8_t buf[256] = { 0 };
+    if (!p.Encode(buf, 256)) {
+        printf("Peron_PhoneNumber encode failed");
+        return;
+    }
+
+    auto p2 = Person_PhoneNumber::Decode(buf, 256);
+    printf("type = %d\n", p2.get_type());
+}
+
 int main(void) {
-    printf("Hello World!\n");
+    person();
+    phone_number();
     return 0;
 }
