fdaca2592a8246d6fa97534fb785e9e02e623f5c
Michael Pratt <michael@pratt.im>
2015-12-13 17:50:11 -0800

    Test get and set

--- tests/cpp/simple_test.cc
+++ tests/cpp/simple_test.cc
@@ -1,7 +1,7 @@
 #include "external/googletest/googletest/include/gtest/gtest.h"
 #include "tests/cpp/simple.pb.hpp"
 
-TEST(SimpleTest, SetAll) {
+TEST(SimpleTest, SetGet) {
     Simple s;
 
     s.set_this_is_required(true);
@@ -13,4 +13,11 @@ TEST(SimpleTest, SetAll) {
     Simple_SubMessage ss;
     ss.set_default_three(5);
     s.set_submessage(ss);
+
+    EXPECT_EQ(true, s.get_this_is_required());
+    EXPECT_EQ(true, s.get_this_is_optional());
+    EXPECT_EQ(42, s.get_an_int());
+    EXPECT_DOUBLE_EQ(2.71, s.get_a_double());
+    EXPECT_EQ(nanopb::Simple_Enum_TWO, s.get_an_enum());
+    EXPECT_EQ(5ULL, s.get_submessage().get_default_three());
 }
